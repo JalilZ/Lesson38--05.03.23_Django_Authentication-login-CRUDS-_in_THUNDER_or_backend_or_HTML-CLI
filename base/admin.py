b9ff605a0f922c97c0d0ed994581f8cda65510c5
unknown <jalil.zoabi@gmail.com>
2023-03-05 21:24:18 +0200

    1

---
+++ base/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Student
+
+admin.site.register(Student)
+
+
+# username: jalil
+# email: jalil@fake.com
+# password: jalilpython
